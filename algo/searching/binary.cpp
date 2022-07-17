c6f595d39f38c378fe43f35e50e3e811c0caa4ce
Jonah Perry <newjonah12348@gmail.com>
2022-07-17 00:10:46 -0400

    ok

--- algo/searching/binary.cpp
+++ algo/searching/binary.cpp
@@ -2,7 +2,18 @@
 #include <vector>
 
 /*
- * Binary search
+ * Let's say you're trying to find a definition for a word in a dictionary.
+ * In a linear fashion, you can start at A and run through it until you get to the target
+ *
+ * Unless your word is on the first few pages of the As, this is extremely inefficient.
+ *
+ * Binary searches are great for this, as they start with the middle element.
+ * If the value is equal to the target, just return that.
+ * If the value is in the lower half, constrain the search to the lower half in the next run
+ * If the value is in the upper half, constrain the search to the higher half in the next run
+ *
+ * Repeatedly do this
+ * Return -1 if the target cannot be found (or whatever the exercise asks you to return)
  */
 
 int binarySearch(std::vector<int> &v, int x) {
--- algo/searching/linear.cpp
+++ algo/searching/linear.cpp
@@ -3,6 +3,9 @@
 
 /*
  * Linear search
+ * This is fairly straightforward. Go through the array from beginning to end.
+ * If the value = the target, return the value
+ * Otheweise, return -1
 */
 int linearSearch(std::vector<int> &v, int x) {
     for(int i = 0; i <= v.size(); i++) {
